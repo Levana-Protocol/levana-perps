4d8174f08fb7c5873697f04e5e0e488f2aec58e4
lvn-rusty-dragon <rusty-dragon@levana.exchange>
2023-05-02 22:22:31 +0300

    cargo fmt

--- contracts/farming/src/state/farming.rs
+++ contracts/farming/src/state/farming.rs
@@ -24,7 +24,10 @@ struct FarmingTotals {
 
 impl FarmingTotals {
     fn xlp_to_farming(&self, xlp: LpToken) -> Result<FarmingToken> {
-        anyhow::ensure!(self.farming.is_zero() == self.xlp.is_zero(), "We must either have no farming and no xLP tokens, or have some of both");
+        anyhow::ensure!(
+            self.farming.is_zero() == self.xlp.is_zero(),
+            "We must either have no farming and no xLP tokens, or have some of both"
+        );
         Ok(if self.farming.is_zero() {
             FarmingToken::from_decimal256(xlp.into_decimal256())
         } else {
