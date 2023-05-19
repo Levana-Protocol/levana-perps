f27a4e9fc3d09d5a6f32437dfe58a09a3cf32da8
lvn-ryu <ryu@levana.exchange>
2023-05-19 13:45:20 +0300

    PERP-1342 | lp sendfrom

--- contracts/liquidity_token/src/state/market.rs
+++ contracts/liquidity_token/src/state/market.rs
@@ -60,18 +60,45 @@ impl State<'_> {
                 amount,
                 msg,
             } => {
+                // This receive message will appear on the destination contract directly from here (the proxy cw20)
+                // as far as that destination contract is concerned, that's the cw20 contract itself giving it funds
                 let send = ReceiverExecuteMsg::Receive(Cw20ReceiveMsg {
                     sender: sender.clone().into(),
                     amount,
                     msg,
                 });
+                // However, the actual balances are kept on the market contract, and so we also need to transfer the balance
+                // from the market to the destination contract
                 let msg = LiquidityTokenExecuteMsg::Transfer {
                     recipient: contract.clone(),
                     amount,
                 };
                 (msg, Some((contract, send)))
             }
-            msg => (msg, None),
+            // SendFrom also needs special handling to ensure the messages to the destination contract come from the proxy, not the market
+            LiquidityTokenExecuteMsg::SendFrom {
+                owner,
+                contract,
+                amount,
+                msg,
+            } => {
+                // same idea as above, the target contract gets receive message saying "cw20 has sent you funds"
+                let send = ReceiverExecuteMsg::Receive(Cw20ReceiveMsg {
+                    sender: sender.clone().into(),
+                    amount,
+                    msg,
+                });
+
+                // but the balance must be updated on the market contract. In this case going through "transfer from"
+                // which will ensure the correct approval gating etc.
+                let msg = LiquidityTokenExecuteMsg::TransferFrom {
+                    owner,
+                    recipient: contract.clone(),
+                    amount,
+                };
+                (msg, Some((contract, send)))
+            }
+            _ => (msg, None),
         };
         ctx.response.add_execute_submessage_oneshot(
             self.market_addr(ctx.storage)?,
