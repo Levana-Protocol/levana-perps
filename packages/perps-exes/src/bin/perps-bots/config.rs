d1eac4ecda33f1df7764d49cb16a288732791b53
lvn-rusty-dragon <rusty-dragon@levana.exchange>
2023-06-01 19:23:11 +0300

    COSMOS_GAS_MULTIPLIER on testnet too

--- packages/perps-exes/src/bin/perps-bots/config.rs
+++ packages/perps-exes/src/bin/perps-bots/config.rs
@@ -100,6 +100,8 @@ impl Opt {
         let (faucet_bot, faucet_bot_runner) =
             FaucetBot::new(faucet_bot_wallet, testnet.hcaptcha_secret.clone(), faucet);
 
+        let gas_multiplier = testnet.gas_multiplier.or(*gas_multiplier);
+
         let testnet = BotConfigTestnet {
             tracker: tracker.with_context(|| format!("No tracker found for {network}"))?,
             faucet,
@@ -165,7 +167,7 @@ impl Opt {
                 None
             },
             watcher: partial.watcher.clone(),
-            gas_multiplier: *gas_multiplier,
+            gas_multiplier,
             pyth_endpoint: pyth_config.endpoint.clone(),
             execs_per_price: partial.execs_per_price,
         };
