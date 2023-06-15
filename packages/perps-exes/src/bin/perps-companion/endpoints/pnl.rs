ae3e65c7bb4a0a1dce99ac2015e740148d8e3f1d
lvn-rusty-dragon <rusty-dragon@levana.exchange>
2023-06-15 15:18:49 +0300

    Add retry logic

--- packages/perps-exes/src/bin/perps-companion/endpoints/pnl.rs
+++ packages/perps-exes/src/bin/perps-companion/endpoints/pnl.rs
@@ -78,15 +78,28 @@ impl MarketContract {
     where
         T: serde::de::DeserializeOwned,
     {
-        self.0.query(&msg).await.map_err(|source| {
-            let e = Error::FailedToQueryContract {
-                source,
-                msg,
-                query_type,
-            };
-            log::error!("{e}");
-            e
-        })
+        let mut attempt = 1;
+        loop {
+            let res = self.0.query(&msg).await.map_err(|source| {
+                let e = Error::FailedToQueryContract {
+                    source,
+                    msg: msg.clone(),
+                    query_type,
+                };
+                log::error!("Attempt #{attempt}: {e}");
+                e
+            });
+            match res {
+                Ok(x) => break Ok(x),
+                Err(e) => {
+                    if attempt >= 5 {
+                        break Err(e);
+                    } else {
+                        attempt += 1;
+                    }
+                }
+            }
+        }
     }
 }
 
