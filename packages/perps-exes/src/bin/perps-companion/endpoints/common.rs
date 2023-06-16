7e9e4ef8adfb915e26c2b612039cfdaf477933a6
lvn-rusty-dragon <rusty-dragon@levana.exchange>
2023-06-16 06:46:57 +0300

    perp-1494 | 404 handler

--- packages/perps-exes/src/bin/perps-companion/endpoints/common.rs
+++ packages/perps-exes/src/bin/perps-companion/endpoints/common.rs
@@ -2,7 +2,9 @@ use axum::{
     http::HeaderValue,
     response::{IntoResponse, Response},
 };
-use reqwest::header::CONTENT_TYPE;
+use reqwest::{header::CONTENT_TYPE, StatusCode};
+
+use super::ErrorPage;
 
 pub(crate) async fn homepage() -> &'static str {
     r#"Welcome intrepid reader!
@@ -31,5 +33,21 @@ pub(crate) async fn robots_txt() -> Response {
     let mut res = include_str!("../../../../static/robots.txt").into_response();
     res.headers_mut()
         .insert(CONTENT_TYPE, HeaderValue::from_static("text/plain"));
+    res
+}
+
+pub(crate) async fn not_found() -> ErrorPage<&'static str> {
+    ErrorPage {
+        error: "Page not found",
+        code: StatusCode::NOT_FOUND,
+    }
+}
+
+pub(super) async fn error_css() -> Response {
+    let mut res = include_str!("../../../../static/error.css").into_response();
+    res.headers_mut().insert(
+        CONTENT_TYPE,
+        HeaderValue::from_static("text/css; charset=utf-8"),
+    );
     res
 }
