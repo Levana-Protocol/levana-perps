2504e47fc9a3657b1f7d2ca0ea42241923f4898d
lvn-rusty-dragon <rusty-dragon@levana.exchange>
2023-10-17 07:18:20 +0300

    perp-2232 | parallelize stats check, fix data storage bug

--- packages/perps-exes/src/bin/perps-bots/app/stale.rs
+++ packages/perps-exes/src/bin/perps-bots/app/stale.rs
@@ -1,13 +1,17 @@
+use std::sync::Arc;
+
 use anyhow::Result;
 use axum::async_trait;
 use chrono::{DateTime, Utc};
+use cosmos::{Address, HasAddress};
+use dashmap::DashMap;
 use msg::prelude::*;
 use perps_exes::{contracts::MarketContract, timestamp_to_date_time};
 
 use crate::{
     config::BotConfigByType,
     util::markets::Market,
-    watcher::{TaskLabel, WatchedTaskOutput, WatchedTaskPerMarket},
+    watcher::{ParallelWatcher, TaskLabel, WatchedTaskOutput, WatchedTaskPerMarketParallel},
 };
 
 use super::{factory::FactoryInfo, App, AppBuilder};
@@ -19,23 +23,28 @@ impl AppBuilder {
             BotConfigByType::Mainnet { .. } => false,
         };
         if !ignore_stale {
-            self.watch_periodic(TaskLabel::Stale, Stale::default())?;
+            self.watch_periodic(TaskLabel::Stale, ParallelWatcher::new(Stale::default()))?;
         }
         Ok(())
     }
 }
 
-#[derive(Clone, Copy, Default)]
+#[derive(Default)]
 struct Stale {
+    markets: Arc<DashMap<Address, StaleMarket>>,
+}
+
+#[derive(Clone, Copy, Default)]
+struct StaleMarket {
     total_checks: u128,
     sum_of_unpends: u128,
     count_nonzero_unpend: u128,
 }
 
 #[async_trait]
-impl WatchedTaskPerMarket for Stale {
+impl WatchedTaskPerMarketParallel for Stale {
     async fn run_single_market(
-        &mut self,
+        self: Arc<Self>,
         _app: &App,
         _factory: &FactoryInfo,
         market: &Market,
@@ -50,25 +59,31 @@ impl WatchedTaskPerMarket for Stale {
 }
 
 impl Stale {
-    async fn check_stale_single(&mut self, market: &MarketContract) -> Result<String> {
+    async fn check_stale_single(&self, market: &MarketContract) -> Result<String> {
         let status = market.status().await?;
         let last_crank_completed = status
             .last_crank_completed
             .context("No cranks completed yet")?;
         let last_crank_completed = timestamp_to_date_time(last_crank_completed)?;
 
-        self.total_checks += 1;
-        self.sum_of_unpends += u128::from(status.unpend_queue_size);
+        let address = market.get_address();
+        let mut stats = self
+            .markets
+            .get(&address)
+            .map_or_else(StaleMarket::default, |x| *x);
+        stats.total_checks += 1;
+        stats.sum_of_unpends += u128::from(status.unpend_queue_size);
         if status.unpend_queue_size > 0 {
-            self.count_nonzero_unpend += 1;
+            stats.count_nonzero_unpend += 1;
         }
+        self.markets.insert(address, stats);
 
         let mk_message = |msg| Msg {
             msg,
             last_crank_completed,
             unpend_queue_size: status.unpend_queue_size,
             unpend_limit: status.config.unpend_limit,
-            stale: self,
+            stale: &stats,
         };
         if status.is_stale() {
             Err(mk_message("Protocol is in stale state").to_anyhow())
@@ -85,7 +100,7 @@ struct Msg<'a> {
     last_crank_completed: DateTime<Utc>,
     unpend_queue_size: u32,
     unpend_limit: u32,
-    stale: &'a Stale,
+    stale: &'a StaleMarket,
 }
 
 impl Display for Msg<'_> {
