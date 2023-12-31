8203a2ca4418bf66b007309880761fbfe062de6b
lvn-ryu <ryu@levana.exchange>
2023-12-31 15:06:38 +0200

    PERP-2804 | more position tests

--- packages/multi_test/tests/multi_test/position/mod.rs
+++ packages/multi_test/tests/multi_test/position/mod.rs
@@ -1,16 +1,15 @@
 pub mod close;
+pub mod congestion;
 pub mod data;
 pub mod infinite_gains;
+pub mod liquidate;
 pub mod max_leverage;
+pub mod minimum_size;
+pub mod misc;
 pub mod open;
 pub mod pnl;
-pub mod liquidate;
-
-/* still failing:
-pub mod minimum_size;
-pub mod update;
 pub mod take_profit;
+/* still failing:
+pub mod update;
 pub mod pending_fees;
-pub mod misc;
-pub mod congestion;
 */
