bc7d2b7c5a0579d0d7d89b25c652174f52cf2c10
lvn-ryu <ryu@levana.exchange>
2024-01-08 17:06:21 +0200

    PERP-2883 | true -> false in watcher

--- packages/perps-exes/src/bin/perps-bots/watcher.rs
+++ packages/perps-exes/src/bin/perps-bots/watcher.rs
@@ -92,7 +92,7 @@ impl TaskLabel {
             TaskLabel::TotalDepositAlert => false,
             TaskLabel::RpcHealth => false,
             TaskLabel::Congestion => false,
-            TaskLabel::HighGas => false,
+            TaskLabel::HighGas => true,
         }
     }
 }
