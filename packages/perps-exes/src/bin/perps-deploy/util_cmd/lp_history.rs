31031984a7b905886ed7f66233e1818bda3a9399
lvn-rusty-dragon <rusty-dragon@levana.exchange>
2024-02-04 16:13:31 +0200

    Remove unneeded actions parsing

--- packages/perps-exes/src/bin/perps-deploy/util_cmd/lp_history.rs
+++ packages/perps-exes/src/bin/perps-deploy/util_cmd/lp_history.rs
@@ -148,14 +148,8 @@ async fn worker(
         struct Record<'a> {
             market_id: &'a MarketId,
             addr: Address,
-            collateral_deposited_lp_info: Collateral,
-            collateral_deposited: Collateral,
-            collateral_withdrawn: Collateral,
-            collateral_remaining: Collateral,
             yield_withdrawn: Collateral,
             yield_pending: Collateral,
-            first_action: Option<chrono::DateTime<chrono::Utc>>,
-            last_action: Option<chrono::DateTime<chrono::Utc>>,
             lp_collateral: Collateral,
             xlp_collateral: Collateral,
             xlp_unstaking: Collateral,
@@ -166,14 +160,8 @@ async fn worker(
         let mut record = Record {
             market_id: &market_id,
             addr: lp,
-            collateral_deposited_lp_info: lp_info.history.deposit,
-            collateral_deposited: Collateral::zero(),
-            collateral_remaining: lp_info.lp_collateral + lp_info.xlp_collateral,
             yield_withdrawn: lp_info.history.r#yield,
             yield_pending: lp_info.available_yield_lp + lp_info.available_yield_xlp,
-            collateral_withdrawn: Collateral::zero(),
-            first_action: None,
-            last_action: None,
             lp_collateral: lp_info.lp_collateral,
             xlp_collateral: lp_info.xlp_collateral,
             xlp_unstaking: lp_info
@@ -186,33 +174,6 @@ async fn worker(
                     )
                 }),
         };
-
-        let actions = market.get_lp_actions(lp).await?;
-        for LpAction {
-            kind,
-            timestamp,
-            tokens: _,
-            collateral,
-            collateral_usd: _,
-        } in actions
-        {
-            match kind {
-                LpActionKind::DepositLp => record.collateral_deposited += collateral,
-                LpActionKind::DepositXlp => record.collateral_deposited += collateral,
-                LpActionKind::ReinvestYieldLp => record.collateral_deposited += collateral,
-                LpActionKind::ReinvestYieldXlp => record.collateral_deposited += collateral,
-                LpActionKind::UnstakeXlp => (),
-                LpActionKind::CollectLp => (),
-                LpActionKind::Withdraw => record.collateral_withdrawn += collateral,
-                LpActionKind::ClaimYield => (),
-            }
-
-            let timestamp = timestamp.try_into_chrono_datetime()?;
-            if record.first_action.is_none() {
-                record.first_action = Some(timestamp);
-            }
-            record.last_action = Some(timestamp);
-        }
 
         let mut csv = csv.lock().await;
         csv.serialize(&record)?;
