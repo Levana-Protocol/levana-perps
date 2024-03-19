a3c9b461c2a06f750832190507fe4f283b0d52ba
lvn-rusty-dragon <rusty-dragon@levana.exchange>
2024-03-19 08:52:28 +0200

    Local Osmosis: use -it to allow signal handling

--- .ci/osmolocal.sh
+++ .ci/osmolocal.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-docker run --rm --name osmolocaltest -p 26657:26657 -p 1317:1317 -p 9090:9090 -p 9091:9091 localosmo:latest
+docker run --rm -it --name osmolocaltest -p 26657:26657 -p 1317:1317 -p 9090:9090 -p 9091:9091 localosmo:latest
