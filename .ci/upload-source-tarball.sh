9f00bfffa7ea957b01e307d7da276a6980298938
lvn-rusty-dragon <rusty-dragon@levana.exchange>
2024-05-01 12:12:24 +0300

    upload-source-tarball.sh helper script

---
+++ .ci/upload-source-tarball.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+pushd source-tarballs
+for f in levana-perps-$1{.tar.gz,-checksums.txt}
+do
+  set -x
+  aws s3 cp $f s3://static.meteors.levana.finance/perps-source/$f
+  curl -i --head https://static.levana.finance/perps-source/$f
+  set +x
+done
+popd
+
+DEST=../docs/src/source-code.md
+echo "| `$1` | [Code](https://static.levana.finance/perps-source/levana-perps-$1.tar.gz) | [Checksums](https://static.levana.finance/perps-source/levana-perps-$1-checksums.txt) |" >> "$DEST"
+echo "Don't forget to push changes for $DEST"
