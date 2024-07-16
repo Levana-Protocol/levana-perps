86925162002483d8538735951ba830797fc63037
Hasky Dragon <hasky-dragon@levana.exchange>
2024-07-16 10:40:10 +0530

    MPA: View historical data

--- packages/perps-exes/src/bin/perps-market-params/routes.rs
+++ packages/perps-exes/src/bin/perps-market-params/routes.rs
@@ -1,4 +1,6 @@
 use axum_extra::routing::TypedPath;
+use serde::Deserialize;
+use shared::storage::MarketId;
 
 #[derive(TypedPath)]
 #[typed_path("/")]
@@ -7,3 +9,9 @@ pub(crate) struct HomeRoute;
 #[derive(TypedPath)]
 #[typed_path("/healthz")]
 pub(crate) struct HealthRoute;
+
+#[derive(TypedPath, Deserialize)]
+#[typed_path("/historical/:market_id")]
+pub(crate) struct HistoryRoute {
+    pub(crate) market_id: MarketId,
+}
--- packages/perps-exes/src/bin/perps-market-params/web.rs
+++ packages/perps-exes/src/bin/perps-market-params/web.rs
@@ -1,4 +1,8 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{
+    collections::{HashMap, HashSet},
+    path::PathBuf,
+    sync::Arc,
+};
 
 use anyhow::{Context, Result};
 use askama::Template;
@@ -13,8 +17,8 @@ use tokio::task::JoinSet;
 
 use crate::{
     cli::{Opt, ServeOpt},
-    market_param::{compute_coin_dnfs, DnfNotify},
-    routes::{HealthRoute, HomeRoute},
+    market_param::{compute_coin_dnfs, load_historical_data, DnfNotify, DnfRecord},
+    routes::{HealthRoute, HistoryRoute, HomeRoute},
 };
 
 pub(crate) async fn axum_main(serve_opt: ServeOpt, opt: Opt) -> Result<()> {
@@ -24,18 +28,22 @@ pub(crate) async fn axum_main(serve_opt: ServeOpt, opt: Opt) -> Result<()> {
 #[derive(Clone)]
 pub(crate) struct NotifyApp {
     pub(crate) market_params: Arc<RwLock<HashMap<MarketId, DnfNotify>>>,
+    pub(crate) markets: Arc<RwLock<HashSet<MarketId>>>,
+    pub(crate) data_dir: PathBuf,
 }
 
 impl NotifyApp {
-    pub(crate) fn new() -> Self {
+    pub(crate) fn new(data_dir: PathBuf) -> Self {
         NotifyApp {
             market_params: Arc::new(RwLock::new(HashMap::new())),
+            markets: Arc::new(RwLock::new(HashSet::new())),
+            data_dir,
         }
     }
 }
 
 async fn main_inner(serve_opt: ServeOpt, opt: Opt) -> Result<()> {
-    let state = Arc::new(NotifyApp::new());
+    let state = Arc::new(NotifyApp::new(serve_opt.cmc_data_dir.clone()));
 
     let mut set = JoinSet::new();
     let dnf_state = state.clone();
@@ -44,6 +52,7 @@ async fn main_inner(serve_opt: ServeOpt, opt: Opt) -> Result<()> {
     let router = axum::Router::new()
         .typed_get(index)
         .typed_get(healthz)
+        .typed_get(history)
         .with_state(state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
@@ -68,6 +77,7 @@ pub(crate) struct NoQueryString {}
 #[template(path = "market_params.html")]
 struct IndexTemplate {
     market_params: HashMap<MarketId, DnfNotify>,
+    markets: HashSet<MarketId>,
 }
 
 pub(crate) async fn index(
@@ -76,8 +86,49 @@ pub(crate) async fn index(
     _: Query<NoQueryString>,
 ) -> axum::response::Response {
     let market_params = app.market_params.read().clone();
-    let index_page = IndexTemplate { market_params }.render();
+    let markets = app.markets.read().clone();
+    let index_page = IndexTemplate {
+        market_params,
+        markets,
+    }
+    .render();
     match index_page {
+        Ok(page) => Html::from(page).into_response(),
+        Err(err) => {
+            let res = Html::from(format!("Failure during template conversion {err}"));
+            (axum::http::StatusCode::INTERNAL_SERVER_ERROR, res).into_response()
+        }
+    }
+}
+
+#[derive(Template, serde::Serialize)]
+#[template(path = "mpa_history.html")]
+struct HistoryTemplate {
+    historical_records: Vec<DnfRecord>,
+    market_id: MarketId,
+}
+
+pub(crate) async fn history(
+    HistoryRoute { market_id }: HistoryRoute,
+    app: State<Arc<NotifyApp>>,
+    _: Query<NoQueryString>,
+) -> axum::response::Response {
+    let historical_data = load_historical_data(&market_id, app.data_dir.clone());
+    let historical_data = match historical_data {
+        Ok(result) => result,
+        Err(err) => {
+            let res = Html::from(format!("Failure during loading historical data: {err}"));
+            return (axum::http::StatusCode::INTERNAL_SERVER_ERROR, res).into_response();
+        }
+    };
+
+    let history_page = HistoryTemplate {
+        historical_records: historical_data.data,
+        market_id,
+    }
+    .render();
+
+    match history_page {
         Ok(page) => Html::from(page).into_response(),
         Err(err) => {
             let res = Html::from(format!("Failure during template conversion {err}"));
