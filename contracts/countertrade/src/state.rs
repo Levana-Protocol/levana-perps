b66d1e8e34a291cfe7e0aff073708a8122f098f4
Hasky Dragon <hasky-dragon@levana.exchange>
2024-07-23 22:13:21 +0530

    Use cursors approach

--- contracts/countertrade/src/state.rs
+++ contracts/countertrade/src/state.rs
@@ -6,6 +6,8 @@ pub(crate) const CONFIG: Item<Config> = Item::new("config");
 /// Shares held per wallet and market
 pub(crate) const SHARES: Map<(&Addr, &MarketId), NonZero<LpToken>> = Map::new("shares");
 
+pub(crate) const SHARES_CURSOR: Item<ResetSharesCursor> = Item::new("reset-shares");
+
 /// Total collateral information per market
 pub(crate) const TOTALS: Map<&MarketId, Totals> = Map::new("totals");
 
