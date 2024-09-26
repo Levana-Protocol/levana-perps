7ce0c1ad97e63e7a4a95883c9e6539223377dad8
Hasky Dragon <hasky-dragon@levana.exchange>
2024-09-26 16:22:16 +0530

    Fix tests

--- packages/multi_test/tests/multi_test/copy_trading.rs
+++ packages/multi_test/tests/multi_test/copy_trading.rs
@@ -1,5 +1,6 @@
 use std::str::FromStr;
 
+use cosmwasm_std::Event;
 use levana_perpswap_multi_test::{market_wrapper::PerpsMarket, PerpsApp};
 use msg::{
     contracts::copy_trading::{QueueItem, QueuePositionId, WorkResp},
@@ -52,10 +53,26 @@ fn initial_no_work() {
 fn detect_process_queue_item_work() {
     let market = PerpsMarket::new(PerpsApp::new_cell().unwrap()).unwrap();
     let trader = market.clone_trader(0).unwrap();
+    let token = market.get_copytrading_token().unwrap();
 
     market
         .exec_copytrading_mint_and_deposit(&trader, "100")
         .unwrap();
+
+    let work = market.query_copy_trading_work().unwrap();
+    // Needs to compute lp token value for the initial deposit
+    assert_eq!(
+        work,
+        WorkResp::HasWork {
+            work_description: msg::contracts::copy_trading::WorkDescription::ComputeLpTokenValue {
+                token
+            }
+        }
+    );
+    let resp = market.exec_copytrading_do_work(&trader).unwrap();
+    assert!(resp.has_event(
+        &Event::new("wasm-lp-token").add_attribute("value", Collateral::one().to_string())
+    ));
 
     let work = market.query_copy_trading_work().unwrap();
     assert_eq!(
@@ -65,7 +82,7 @@ fn detect_process_queue_item_work() {
                 id: QueuePositionId::new(0)
             }
         }
-    )
+    );
 }
 
 #[test]
@@ -77,8 +94,15 @@ fn do_actual_deposit() {
         .exec_copytrading_mint_and_deposit(&trader, "100")
         .unwrap();
 
-    // Process queue item: do the actual deposit
+    // Compute LP token value
     market.exec_copytrading_do_work(&trader).unwrap();
+    // Process queue item: do the actual deposit
+    let resp = market.exec_copytrading_do_work(&trader).unwrap();
+    assert!(resp.has_event(
+        &Event::new("wasm-deposit")
+            .add_attribute("funds", "100".to_owned())
+            .add_attribute("shares", "100".to_owned())
+    ));
 
     // Should not find any work now
     let work = market.query_copy_trading_work().unwrap();
@@ -86,15 +110,16 @@ fn do_actual_deposit() {
 }
 
 #[test]
-fn detect_compute_lp_token_work() {
+fn does_not_compute_lp_token_work() {
     let market = PerpsMarket::new(PerpsApp::new_cell().unwrap()).unwrap();
-    let token = market.get_copytrading_token().unwrap();
     let trader = market.clone_trader(0).unwrap();
 
     market
         .exec_copytrading_mint_and_deposit(&trader, "100")
         .unwrap();
 
+    // Compute initial LP token value
+    market.exec_copytrading_do_work(&trader).unwrap();
     // Process queue item: do the actual deposit
     market.exec_copytrading_do_work(&trader).unwrap();
 
@@ -107,15 +132,16 @@ fn detect_compute_lp_token_work() {
         .exec_copytrading_mint_and_deposit(&trader, "100")
         .unwrap();
 
+    // Should not compute LP token value since there has been no positions opened etc.
     let work = market.query_copy_trading_work().unwrap();
     assert_eq!(
         work,
         WorkResp::HasWork {
-            work_description: msg::contracts::copy_trading::WorkDescription::ComputeLpTokenValue {
-                token
+            work_description: msg::contracts::copy_trading::WorkDescription::ProcessQueueItem {
+                id: QueuePositionId::new(1)
             }
         }
     );
-    // Process queue item: compute lp token value
+    // Process queue item: Do actual deposit
     market.exec_copytrading_do_work(&trader).unwrap();
 }
