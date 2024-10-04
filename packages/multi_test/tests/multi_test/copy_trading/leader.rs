03d6e03c4719fd1d8900f41231be18386a3e1566
Hasky Dragon <hasky-dragon@levana.exchange>
2024-10-04 10:22:25 +0530

    Add test for the case

--- packages/multi_test/tests/multi_test/copy_trading/leader.rs
+++ packages/multi_test/tests/multi_test/copy_trading/leader.rs
@@ -201,3 +201,59 @@ fn leader_incorrect_position() {
     let work = market.query_copy_trading_work().unwrap();
     assert_eq!(work, WorkResp::NoWork);
 }
+
+#[test]
+fn leader_open_position_compute_token() {
+    // This simulates this scenario: Deposit, withdrawal, Open
+    // position and then deposit again. Before the final deposit, LP
+    // value computation should happen again.
+    let market = PerpsMarket::new(PerpsApp::new_cell().unwrap()).unwrap();
+    let trader = market.clone_trader(0).unwrap();
+    let lp = market.clone_lp(0).unwrap();
+
+    load_markets(&market);
+    deposit_money(&market, &trader, "200");
+    withdraw_money(&market, &trader, "10");
+
+    let status = market.query_copy_trading_leader_tokens().unwrap();
+    let tokens = status.tokens;
+    assert_eq!(tokens[0].collateral, "190".parse().unwrap());
+
+    // Leader opens a position
+    market
+        .exec_copy_trading_open_position("10", DirectionToBase::Long, "1.5")
+        .unwrap();
+
+    // Process queue item: Open the position
+    market.exec_copytrading_do_work(&trader).unwrap();
+    market.exec_crank_till_finished(&lp).unwrap();
+
+    let position_ids = market
+        .query_position_token_ids(&market.copy_trading_addr)
+        .unwrap();
+    let position_ids = position_ids
+        .iter()
+        .map(|item| PositionId::new(item.parse().unwrap()))
+        .collect::<Vec<_>>();
+    assert!(position_ids.len() == 1);
+
+    let work = market.query_copy_trading_work().unwrap();
+    assert_eq!(
+        work,
+        WorkResp::HasWork {
+            work_description: WorkDescription::HandleDeferredExecId {}
+        }
+    );
+
+    // Process queue item: Handle deferred exec id
+    market.exec_copytrading_do_work(&trader).unwrap();
+
+    let work = market.query_copy_trading_work().unwrap();
+    assert_eq!(work, WorkResp::NoWork);
+
+    deposit_money(&market, &trader, "50");
+
+    let status = market.query_copy_trading_leader_tokens().unwrap();
+    let tokens = status.tokens;
+    assert_eq!(tokens[0].collateral, "230".parse().unwrap());
+}
