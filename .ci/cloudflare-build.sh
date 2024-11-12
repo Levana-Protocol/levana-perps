31057e61cf6a4b69ae5abf25180ff541d3412317
Sibi Prabakaran <sibi@psibi.in>
2024-11-12 17:01:15 +0530

    Update build

--- .ci/cloudflare-build.sh
+++ .ci/cloudflare-build.sh
@@ -2,7 +2,10 @@
 
 set -euo pipefail
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+NEW_PATH="$HOME/.cargo/bin"
+export PATH="$PATH:$NEW_PATH"
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
 cargo bininstall just@1.31.0
