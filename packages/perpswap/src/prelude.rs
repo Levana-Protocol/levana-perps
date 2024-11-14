9a7627bcb54cb061fafdc81e08395a60fe266258
Sibi Prabakaran <sibi@psibi.in>
2024-11-14 14:28:51 +0530

    Remove perps_error_data macro

--- packages/perpswap/src/prelude.rs
+++ packages/perpswap/src/prelude.rs
@@ -24,10 +24,7 @@ pub use crate::{
     auth::*,
     storage::{external_map_has, load_external_item, load_external_map},
 };
-pub use crate::{
-    error::*, perp_anyhow, perp_anyhow_data, perp_bail, perp_bail_data, perp_ensure,
-    perp_error_data,
-};
+pub use crate::{error::*, perp_anyhow, perp_anyhow_data, perp_bail, perp_bail_data, perp_ensure};
 
 pub use anyhow::{anyhow, bail, Context, Result};
 pub use cosmwasm_schema::cw_serde;
