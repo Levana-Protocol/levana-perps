26bdc54109e551f9f2a87dbce5ac2fd284ed0119
Sibi Prabakaran <sibi@psibi.in>
2024-11-19 09:20:27 +0530

    Remove ErrorId from import as it's unused

--- packages/perpswap/src/auth.rs
+++ packages/perpswap/src/auth.rs
@@ -1,6 +1,6 @@
 use std::fmt::Display;
 
-use crate::error::{ErrorDomain, ErrorId, PerpError};
+use crate::error::{ErrorDomain, PerpError};
 use crate::storage::load_external_item;
 use crate::namespace;
 use anyhow::{anyhow, Result};
