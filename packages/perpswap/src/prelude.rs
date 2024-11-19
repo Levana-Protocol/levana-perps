986f384bcf8f9a7313f18f5ad501ac1d421a4627
Sibi Prabakaran <sibi@psibi.in>
2024-11-19 09:46:19 +0530

    cargo fmt

--- packages/perpswap/src/prelude.rs
+++ packages/perpswap/src/prelude.rs
@@ -8,6 +8,7 @@ pub use crate::contracts::market::entry::{
 pub use crate::cosmwasm::*;
 pub use crate::direction::{DirectionToBase, DirectionToNotional};
 pub use crate::error::market::{CongestionReason, MarketError, TriggerPriceMustBe, TriggerType};
+pub use crate::error::*;
 pub use crate::event::CosmwasmEventExt;
 pub use crate::leverage::*;
 pub use crate::market_type::{MarketId, MarketType};
@@ -21,7 +22,6 @@ pub use crate::{
     auth::*,
     storage::{external_map_has, load_external_item, load_external_map},
 };
-pub use crate::error::*;
 
 pub use anyhow::{anyhow, bail, Context, Result};
 pub use cosmwasm_schema::cw_serde;
