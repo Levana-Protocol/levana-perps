343bf6130707656d15cd5cb4337553f17d0f2e5f
Erik Balogh <thisiserikbalogh@gmail.com>
2024-12-04 09:33:37 +0100

    PERP-4136 feat: update localosmosis version which has updated setup

--- .ci/osmolocal.sh
+++ .ci/osmolocal.sh
@@ -9,4 +9,4 @@ else
   FLAG="-it"
 fi
 
-docker run --rm $FLAG --name osmolocaltest -p 26657:26657 -p 1317:1317 -p 9090:9090 -p 9091:9091 ghcr.io/levana-protocol/localosmosis:e039701eea59c6576009814fefd3c1b73f7666ec
+docker run --rm $FLAG --name osmolocaltest -p 26657:26657 -p 1317:1317 -p 9090:9090 -p 9091:9091 ghcr.io/levana-protocol/localosmosis:52fe4e899accde76d7915ea61f4fe8ece3f92a7c
