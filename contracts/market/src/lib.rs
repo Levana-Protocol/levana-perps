71a6cd6678de88858d67645adeb8c87065ec1f00
Sibi Prabakaran <sibi@psibi.in>
2024-12-13 09:16:25 +0530

    Add infrastructure for injecting failures

--- contracts/market/src/lib.rs
+++ contracts/market/src/lib.rs
@@ -1,7 +1,24 @@
 #![deny(clippy::as_conversions)]
 
+use anyhow::bail;
+
 pub mod constants;
 pub mod contract;
 mod deferred_exec;
 pub(crate) mod prelude;
 pub mod state;
+
+/// Injects failure in dev, no-op in prod.
+#[cfg(debug_assertions)]
+pub fn inject_failures_during_test() -> anyhow::Result<()> {
+    let env = std::env::var("LEVANA_CONTRACTS_INJECT_FAILURE");
+    if env.is_ok() {
+        bail!("Injected failure during testing")
+    }
+    Ok(())
+}
+
+#[cfg(not(debug_assertions))]
+pub fn inject_failures_during_test() -> anyhow::Result<()> {
+    Ok(())
+}
