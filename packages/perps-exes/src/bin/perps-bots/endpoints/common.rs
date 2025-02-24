a91436ceb4ffa103d86ed28218006628d2e005a0
Michael Snoyman <michael@snoyman.com>
2025-02-24 12:36:58 +0200

    Bots: redirect for homepage

--- packages/perps-exes/src/bin/perps-bots/endpoints/common.rs
+++ packages/perps-exes/src/bin/perps-bots/endpoints/common.rs
@@ -1,9 +1,17 @@
-pub(crate) async fn homepage() -> &'static str {
-    r#"Welcome intrepid reader!
+use axum::response::{IntoResponse, Response};
+use http::{header::LOCATION, HeaderValue, StatusCode};
+
+pub(crate) async fn homepage() -> Response {
+    let mut res = r#"Welcome intrepid reader!
     
 Not sure what you thought you'd find, but you didn't find it.
 
 Better luck next time."#
+        .into_response();
+    *res.status_mut() = StatusCode::FOUND;
+    res.headers_mut()
+        .insert(LOCATION, HeaderValue::from_static("/status"));
+    res
 }
 
 pub(crate) async fn healthz() -> &'static str {
