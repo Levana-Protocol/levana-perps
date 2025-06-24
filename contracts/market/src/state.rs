b7b8a99f08bf4c61ecf26463ad4fdeecca3cea22
anakinzhed <norys07ss@gmail.com>
2025-06-24 11:14:07 -0500

    PERP-4550 | Special gRPC query for RUNE price

--- contracts/market/src/state.rs
+++ contracts/market/src/state.rs
@@ -12,6 +12,7 @@ pub(crate) mod meta;
 pub(crate) mod order;
 pub(crate) mod position;
 pub(crate) mod referral;
+pub(crate) mod rujira_network;
 #[cfg(feature = "sanity")]
 pub(crate) mod sanity;
 pub(crate) mod shutdown;
